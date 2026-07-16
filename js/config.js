f715077a12e2bd719aa34b5180231c924535d29a
alicetletech-afk <kaitlynn.ont@gmail.com>
2026-07-16 11:03:15 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.PTCAD_CONFIG={
-  apiUrl:"",
+  apiUrl:"{"success":true,"message":"PTCAD Sales Hub API is running","spreadsheetUrl":"https://docs.google.com/spreadsheets/d/1U7GHsN_rUEt9Ao1ohEqzx11ocDqSCDDeJEOPzMohONY/edit"}",
   logoUrl:"https://ptcad.alicetletech.com/assets/images/ptcad-logo.png",
   siteName:"PTCAD Sales Hub",
   demoMode:true,
